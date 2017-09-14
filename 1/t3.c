f22a95430c974ad886972fd29a88d91bf6664cbc
ktmah <ktmah@asb9700u-b08.csil.sfu.ca>
2017-09-14 12:06:55 -0700

    t3 submission

---
+++ 1/t3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( void )
+{
+	float f = 0;
+
+	printf("Enter a number: ");
+
+	scanf( "%f", &f);
+
+	int a = floor(f);
+	int b = round(f);
+	int c = ceil(f);
+
+	printf("%d %d %d\n", a, b, c );
+
+	return 0;
+}
