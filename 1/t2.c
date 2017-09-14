ce2b6617fbae95a5f92a06a78ad91e2dccdbd2a2
ktmah <ktmah@asb9804u-b07.csil.sfu.ca>
2017-09-14 11:20:58 -0700

    t2 submission

--- 1/t2.c
+++ 1/t2.c
@@ -2,7 +2,7 @@
 
 int main ( void )
 {
-	printf( "%lu %lu %lu %lu %lu %lu %lu %lu %lu\n", sizeof(char)*8, sizeof(unsigned char)*8, sizeof(int)*4, sizeof(unsigned int)*4, sizeof(long)*4, sizeof(unsigned long)*4, sizeof(float)*8, sizeof(double)*8, sizeof(long double)*8 );
+	printf( "%lu %lu %lu %lu %lu %lu %lu %lu %lu\n", sizeof(char)*8, sizeof(unsigned char)*8, sizeof(int)*8, sizeof(unsigned int)*8, sizeof(long)*8, sizeof(unsigned long)*8, sizeof(float)*8, sizeof(double)*8, sizeof(long double)*8 );
 
 	return 0;
 }
