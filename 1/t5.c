cc5172923537f0fb63ca97e90975928e9b99fa82
kevin <kmah888@msn.com>
2017-09-18 01:16:09 -0700

    t5 submission

--- 1/t5.c
+++ 1/t5.c
@@ -4,23 +4,19 @@
 int main( void )
 {
 	float f = 0;
-
-	while ( f != EOF )
-	{	
-		int s = 0;
-
-		s = scanf( "%f", &f);
-
-		int a = floor(f);
-		int b = round(f);
-		int c = ceil(f);
-
-		printf("%d %d %d\n", a, b, c );
+	int s = 0;
+	s = scanf( "%f", &f);
+	int a = floor(f);
+	int b = round(f);
+	int c = ceil(f);
 	
-		if ( s == -1 )
-		{
-			printf("Done.\n");
-		}	
+	if ( s == -1)
+	{
+		printf("Done.\n");	
 	}
-	
+	else
+	{
+		printf("%d %d %d\n", a, b, c );
+		main ();		
+	}
 }
