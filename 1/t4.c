d838310eb5557679028e8fa7cd0271854b9b5f63
kevin <kmah888@msn.com>
2017-09-17 21:44:15 -0700

    t4 submission

---
+++ 1/t4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( void )
+{
+ float f = 0;
+ 
+ int s = 0;
+ 
+ s = scanf( "%f", &f);
+
+ int a = floor(f);
+ int b = round(f);
+ int c = ceil(f);	
+
+ if ( s == 1 )
+ {
+  printf("%d %d %d\n", a, b, c ); 
+ }
+
+ else
+ {
+  printf( "scanf error: (%d)\n", s);	
+ }	
+ return 0;
+}
