93630e5172852d977dad5988d224eed14ecefdab
kevin <kmah888@msn.com>
2017-09-27 19:43:12 -0700

    reverse is working

--- 2/reverse.c
+++ 2/reverse.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void reverse( int arr[], unsigned int len );
+void reverse( int arr[], unsigned int len )
 {
 	int tmp = 0;
-	for ( i=0; i<len/2; i++)
+	for ( unsigned i=0; i<len/2; i++)
 	{
 		tmp = arr[i];
 		arr[i] = arr[len-1-i];
 		arr[len-1-i] = tmp;
 	}
 }
+
