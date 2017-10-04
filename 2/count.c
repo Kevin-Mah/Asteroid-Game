045734dddc959c605e95430d4eb6616f023c1aa2
ktmah <ktmah@asb9838nu-c06.csil.sfu.ca>
2017-10-04 13:07:40 -0700

    count submit

--- 2/count.c
+++ 2/count.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main(void)
 {
 	long int charcount = 0;
 	long int linecount = 0;
 	long int wordcount = 0;
 	char c;
+	int letter = false;
 
 	while ( c != EOF )
 	{
@@ -15,10 +16,17 @@ int main(void)
 			break;
 		}
 		charcount++;
-		wordcount++;
 		if ((c>='A' && c<='Z') ||(c>='a' && c<='z')|| c=='\'')
 		{
-			wordcount--;
+			letter = false;
+		}
+		else 
+		{
+			if (letter == false)
+			{
+				wordcount++;
+			}
+			letter = true;
 		}
 		if (c == '\n')
 		{
