1e63c2d603f84280eb3bbbdeab10975b244562df
kevin <kmah888@msn.com>
2017-10-03 18:30:55 -0700

    task1 submit

--- 2/funcs.h
+++ 2/funcs.h
@@ -1,17 +1,5 @@
-int max( int x, int y)
-{
-  if( x > y ) 
-  {
-    return x;
-  } 
-  return y;
-}
+int max( int x, int y);
 
-int min( int x, int y)
-{
-  if( x < y ) 
-  {
-    return x;
-  } 
-  return y;
-}
+
+int min( int x, int y);
+
