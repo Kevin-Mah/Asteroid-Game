6c42b4c580daf7f4f17f5c777b7af9355a5eff68
kevin <kmah888@msn.com>
2017-10-07 17:36:05 -0700

    censored submit

--- 2/censored.c
+++ 2/censored.c
@@ -9,12 +9,19 @@ int main(int argc, char *argv[])
     {
         int replace = 0;
         for (int i = 1; i < argc ; i++)
-            if (strcmp(word, argv[i]) == 0)
+	{
+        	if (strcmp(word, argv[i]) == 0)
+		{
                 replace = 1;
-
+		}
+	}
         if (replace == 1)
-            printf("CENSORED ");
+	{
+        	printf("CENSORED ");
+	}
         else
-            printf("%s ", word);
+	{
+        	printf("%s ", word);
+	}
     }
 }
