fd646e55ad87d8845853f8bdc4b1ccc9c62e4564
kevin <kmah888@msn.com>
2017-10-07 18:11:57 -0700

    contains submit

---
+++ 2/contains.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv[] )
+{
+	int len1 = strlen(argv[1]);
+	int len2 = strlen(argv[2]);
+	char phrase = strstr(argv[1], argv[2]); 
+
+	if (phrase == NULL)
+	{
+		printf("false");
+	}
+	else
+	{	
+		printf("true");
+	}
+return 0;
+}
+	
+
+
