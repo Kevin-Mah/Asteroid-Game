24e6bd5c511236bd67fa327cfe2f09c6ca0226d9
kevin <kmah888@msn.com>
2017-10-09 14:54:39 -0700

    rect submit

--- 2/inrect.c
+++ 2/inrect.c
@@ -4,9 +4,33 @@
 
 int InRectangle( float pt[2], float rect[4] )
 {
-	if ((pt[0] >= rect[0]) && (pt[0] <= rect[2]))
+	float smallx, smally, largex, largey;
+
+	if (rect[2]>rect[0])
 	{
-		if ((pt[1] >= rect[1]) && (pt[1] <= rect[3]))
+		smallx = rect[0];
+		largex = rect[2];
+	}
+	else
+	{
+		smallx = rect[2];
+		largex = rect[0];	
+	}
+
+	if (rect[3]>rect[1])
+	{
+		smally = rect[1];
+		largey = rect[3];
+	}
+	else
+	{
+		smally = rect[3];
+		largey = rect[1];	
+	}
+
+	if ((pt[0] >= smallx) && (pt[0] <= largex))
+	{
+		if ((pt[1] >= smally) && (pt[1] <= largey))
 		{
 			return 1;
 		}
