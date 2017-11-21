cf0e224c81649a2b78eb300c57e86370693974e9
kevin <kmah888@msn.com>
2017-11-20 18:18:16 -0800

    task 1

---
+++ 8/t1.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "point_array.h"
+
+
+
+//initialize array struct
+void point_array_init( point_array_t* pa )
+{
+	pa->len = 0;
+	pa->points = realloc(pa->points, sizeof(point_t));
+	pa->reserved = 0;
+}
+
+//reset the array to empty
+void point_array_reset( point_array_t* pa )
+{
+	pa->len = 0;
+	pa->points = realloc(pa->points, 0);
+	pa->reserved = 0;
+}
+
+// Append a point to the end of an array. If successful, return 0,
+// else return 1;
+int point_array_append( point_array_t* pa, point_t* p )
+{
+	if(pa == NULL || p == NULL)
+	{
+		return 1;
+	}
+	
+	pa->len = pa->len+1;
+	pa->points = realloc(pa->points, sizeof(point_t)*pa->len);
+	
+	pa->points[pa->len-1].x = p->x;
+	pa->points[pa->len-1].y = p->y;
+	pa->points[pa->len-1].z = p->z;
+	return 0;
+}
+
+
+// Remove the point at index i from the array, reducing the size of
+// the array by one. The order of points in the array may change.
+int point_array_remove( point_array_t* pa, unsigned int i )
+{
+	pa->len = pa->len-1;
+	pa->points[i].x = pa->points[pa->len].x;
+	pa->points[i].y = pa->points[pa->len].y;
+	pa->points[i].z = pa->points[pa->len].z;
+	pa->points = realloc( pa->points, sizeof(point_t)*pa->len);
+}
