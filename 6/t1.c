032a7ed04e6989f9710cdd530ca09e4b01dd1c14
kevin <kmah888@msn.com>
2017-11-30 19:02:39 -0800

    task 1

--- 6/t1.c
+++ 6/t1.c
@@ -23,7 +23,7 @@ int intarr_save_binary( intarr_t* ia, const char* filename )
 
 intarr_t* intarr_load_binary( const char* filename )
 {
-	if ( filename == NULL)
+	if ( filename == NULL || !filename)
 	{
 		return NULL;
 	}
